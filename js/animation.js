09cbfd9a05195ef20516c7479a8013ba60d8d64b
VitorRamires <vmr.rs@hotmail.com>
2023-11-13 15:35:38 -0300

    animations

---
+++ js/animation.js
@@ -0,0 +1,17 @@
+
+const elementosAnimados = document.querySelectorAll('.anime')
+const windowHeight = window.innerHeight * 0.63
+
+elementosAnimados[0].classList.add('animeAtivo')
+
+function animePage(){
+  elementosAnimados.forEach(elemento=>{
+    const elementoDistancia = elemento.getBoundingClientRect().top
+    console.log(elementoDistancia)
+    if(elementoDistancia < windowHeight){
+      elemento.classList.add('animeAtivo')
+    }
+  })
+}
+
+window.addEventListener('scroll', animePage)
