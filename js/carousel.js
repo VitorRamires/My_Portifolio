6c7a1b72dd6c4638747409194b648f4c1c9888a8
Vitor Ramires <vmr.rs@hotmail.com>
2024-04-25 09:15:57 -0300

    ajustando

--- js/carousel.js
+++ js/carousel.js
@@ -4,13 +4,10 @@ const btnAfter = document.querySelector(".after");
 const slides = Array.from(document.querySelectorAll(".projeto"));
 let slideCount = 0;
 
-// criar slide diretamente pelo JS, tirar do HTML. 
-
 function handleSlide() {
   function nextItem() {
     slideCount++;
-    slides.forEach((item, index) => {
-      console.log(slides[index]);
+    slides.forEach((item) => {
       item.style.transform = `translate(${-slideCount * 100}%)`;
       if (slideCount > slides.length - 1) {
         slideCount = 0;
