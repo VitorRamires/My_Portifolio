27ae898c33d97a1c92714f4dc151bce81df0c8e6
Vitor Ramires <vmr.rs@hotmail.com>
2024-10-17 17:18:15 -0300

    new portifolio redesign

--- js/animation.js
+++ js/animation.js
@@ -1,14 +1,84 @@
-const elementosAnimados = document.querySelectorAll('.anime')
-const windowHeight = window.innerHeight * 0.75
-elementosAnimados[0].classList.add('animeAtivo')
+const stars = document.querySelectorAll(".banner-animation span");
+const delayElements = document.querySelectorAll(".delay");
+const projectScreen = document.querySelector(".tela-projeto");
 
-function animePage(){
-  elementosAnimados.forEach(elemento=>{
-    const elementoDistancia = elemento.getBoundingClientRect().top
-    if(elementoDistancia < windowHeight){
-      elemento.classList.add('animeAtivo')
+const expCard = document.querySelectorAll(".exp-card");
+const expDescription = document.querySelectorAll(".exp-descricao");
+
+const menuLinks = document.querySelectorAll("header a");
+const allSections = document.querySelectorAll("section");
+
+const hamburguerBtn = document.querySelector(".hamburguer");
+const closeBtn = document.querySelector(".close");
+const btnMenuMobile = document.querySelector(".btn-menu-mobile");
+const menuMobile = document.querySelector(".menu-links-mobile");
+
+stars.forEach((star) => {
+  let positionX = Math.floor(Math.random() * 100);
+  let positionY = Math.floor(Math.random() * 100);
+
+  star.style.left = `${positionX}%`;
+  star.style.top = `${positionY}%`;
+});
+
+menuLinks.forEach((link) => {
+  link.addEventListener("click", (event) => {
+    event.preventDefault();
+    let getSection = document.querySelector(link.getAttribute("href"));
+    let getDistance = getSection.offsetTop;
+
+    if (getSection.classList.contains("tecnologias")) {
+      getDistance = getSection.offsetTop - 400;
+    } else {
+      getDistance = getSection.offsetTop;
     }
-  })
+    menuMobile.classList.remove("active-menu");
+    closeBtn.classList.remove("menu-mobile-action");
+    hamburguerBtn.classList.add("menu-mobile-action");
+    window.scroll({
+      top: getDistance,
+      bottom: 0,
+      behavior: "smooth",
+    });
+  });
+});
+
+function removeClass() {
+  menuLinks.forEach((menu) => {
+    menu.classList.remove("active-link");
+  });
 }
 
-window.addEventListener('scroll', animePage)
+const classLinkAdd = (entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      let actualLink = document.querySelector(
+        `.menu-links a[href="#${entry.target.id}"]`
+      );
+      removeClass();
+      actualLink.classList.add("active-link");
+    }
+  });
+};
+
+const options = {
+  threshold: 0.35,
+};
+
+const observer = new IntersectionObserver(classLinkAdd, options);
+
+allSections.forEach((section) => {
+  observer.observe(section);
+});
+
+btnMenuMobile.addEventListener("click", () => {
+  if (menuMobile.classList.contains("active-menu")) {
+    menuMobile.classList.remove("active-menu");
+    hamburguerBtn.classList.add("menu-mobile-action");
+    closeBtn.classList.remove("menu-mobile-action");
+  } else {
+    menuMobile.classList.add("active-menu");
+    hamburguerBtn.classList.remove("menu-mobile-action");
+    closeBtn.classList.add("menu-mobile-action");
+  }
+});
